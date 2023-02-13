d493cb58f1a97412469e833c2cf00f978cbbc170
Ejajkhan613 <Lenovo@EJAJULANSARI.EJAJULANSARI>
2023-02-13 21:46:00 +0530

    done

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ GitHubCalendar(".calendar", "Ejajkhan613", { responsive: true });
 
 
 // Smooth drop
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth"})
-  })
-})
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth"})
+//   })
+// })

a69ee17f313cf77a8bbdcdcb6948a21f08faa96f
Ejajkhan613 <Lenovo@EJAJULANSARI.EJAJULANSARI>
2023-02-13 21:10:07 +0530

    done

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var typed = new Typed("#auto-type", {
 document.querySelector(".resume").addEventListener("click", () => {
   window.open("./Ejajul_Ansari_Resume.pdf")
 });
-document.getElementById(".second").addEventListener("click", () => {
-  window.open("./Ejajul_Ansari_Resume.pdf")
-});
+// document.getElementById(".second").addEventListener("click", () => {
+//   window.open("./Ejajul_Ansari_Resume.pdf")
+// });
 
 
 
